7a489897fe72ac9fde00d0c2985576c0f8eeed81
mashour1 <mashour@united-internet.de>
2018-05-17 15:54:09 +0200

    nothing

---
+++ data.py
@@ -0,0 +1,27 @@
+def Articles():
+	articles = [
+
+		{
+		'id':1,
+		'title': 'Article One',
+		'body':'fgvdkljkl kljr kerj krej kjejrg kelj r klejr',
+		'author': 'me',
+		'create_date':'08.05.2018'
+		},
+		{
+		'id':2,
+		'title': 'Article Two',
+		'body':'h er rd er terop eoitee tieru i euieruit',
+		'author': 'me',
+		'create_date':'08.05.2018'
+		},
+		{
+		'id':3,
+		'title': 'Article Three',
+		'body':'h er rd er terop eoitee tieru igfnf euieruit',
+		'author': 'me',
+		'create_date':'08.05.2018'
+		}
+	]
+
+	return articles
